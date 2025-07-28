05c27749609d4535797b67240c1d6853c0d83a0b
Abdul Wasay <abdulwasaychoa@gmail.com>
2025-07-28 04:17:33 -0700

    add num1, num2, operator variables that change. when I have num1 after receiving op then set num2 and evaluate the result. number string sets the numbers. there are few problems: clear is not working, code is not clean and clear

--- script.js
+++ script.js
@@ -1,1 +1,103 @@
-console.log("Hello World")
+// Operation functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Variables
+let num1 = null, num2 = null, operator, result;
+
+// DOM elements
+const display = document.querySelector('input.display')
+const numbers = document.querySelector('.numbers')
+const operations = document.querySelector('.operations')
+
+numbers.addEventListener('click', setNumber)
+numbers.addEventListener('click', clearEvaluate)
+operations.addEventListener('click', setOperator)
+operations.addEventListener('click', evaluateOperation)
+
+let numberString = ""
+function setNumber(e) {
+    const value = Number(e.target.value)
+    if(!isNaN(value)) {
+        numberString += value
+        display.value = numberString
+    }
+
+    if (!hasOperator()) {
+        num1 = Number(numberString)
+    } else {
+        num2 = Number(numberString)
+    }
+
+    display.scrollLeft = display.scrollWidth
+}
+
+function clearEvaluate(e) {
+    // Other operations
+    switch(e.target.value) {
+        case "clear":
+            clearDisplay();
+            break;
+        case "evaluate":
+            evaluateOperation();
+            break;
+    }
+}
+
+function setOperator(e) {
+    const value = e.target.value
+    if (value !== undefined) {
+        operator = value
+    }
+    
+    numberString = ""
+}
+
+function hasOperator() {
+    return (operator !== undefined)
+}
+
+function clearDisplay() {
+    num1 = 0;
+    num2 = 0;
+    result = 0;
+    display.value = 0
+}
+
+function evaluateOperation() {
+    if(num1 && num2 && operator) {
+        switch(operator) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+            case '/':
+                result = divide(num1, num2);
+                break;
+        }
+    }
+
+    if (result) {
+        num1 = result
+        num2 = 0
+        display.value = result
+    }
+}
